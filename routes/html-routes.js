ac3939fdcfd90e9534c6f7a8ebe77274547783c9
nkelliny1 <nkelliny@outlook.com>
2020-04-08 12:30:41 -0400

    Added middleware folder

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,6 +1,17 @@
 var path = require("path");
+var isAuthenticated = require("../config/middleware/isAuthenticated");
+
 module.exports = function (app) {
     app.get("/", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/index.html"));
     });
+    app.get("/dashboard", isAuthenticated, function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/dashboard.html"));
+      });
+      app.get("/inventory", isAuthenticated, function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/inventory.html"));
+      });
+      app.get("/requests", isAuthenticated, function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/requests.html"));
+      });
 };
